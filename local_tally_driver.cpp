bdaeccadc2517c805c2b1f62fc71f572ee692de9
Joshua Garbi <joshua.garbi@wallawalla.edu>
2023-04-20 22:38:50 -0700

    addinglibraries

--- local_tally_driver.cpp
+++ local_tally_driver.cpp
@@ -1,6 +1,16 @@
 //main driver for windows/mac running atem control software
-#include "Switchers X.Y\Win\Include\BMDSwitcherAPI.idl"
-#include "Switchers X.Y/macOS/Include/BMDSwitcherAPI.h"
+// WARNING: some errors might occur due to lack of default arduino IDE libraries
+
+// --   dited: Josh Garbi  --
+// --   Date: 4/20/23      --
+
+// Notes: Review SDK dev Kit for additional libraries/packages/APIs
+
+#include "<enter_file_path>\Blackmagic ATEM Switchers SDK 9.0.1\Mac OS X\include\BMDSwitcherAPI.idl"
+#include "<enter_file_path>/Blackmagic ATEM Switchers SDK 9.0.1/Windows/include/BMDSwitcherAPI.h"
+#include "usb.h"
+#include <SPI.h>
+#include <Wire.h>
 
 int main{
     IBMDSwitcherDiscovery* switcherDiscovery = CreateBMDSwitcherDiscoveryInstance();
